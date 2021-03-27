1ac85dc6d286dc73d45806b78b922e1910a478c3
Ryan <ryan.zheng.1998@gmail.com>
2021-03-27 15:02:57 +0800

    half done

--- src/components/Mask.tsx
+++ src/components/Mask.tsx
@@ -1,28 +1,40 @@
 import React from 'react'
 import { Motion, PlainStyle, presets, spring } from 'react-motion'
+import rawData from '../assets/name.json'
+
+interface State {
+    currentStroke: number,
+    
+}
 
 
 const Mask: React.FC = () => {
+    const [currentStroke, setCurrentStroke] = React.useState(0)
 
-    const animatedMask = ({offset}: PlainStyle) => (
-        <mask id="mask0">
-            <path
-                fill="none"
-                strokeWidth="6.79332638"
-                stroke="white"
-                d="M 6.1692973,10.823997 21.256,19.9 v 0"
-                pathLength="100"
-                strokeDasharray="100 100"
-                strokeDashoffset={offset}              
-            />
-        </mask> 
+    const animatedMasks = ({offset}: PlainStyle) => (
+        <>
+            {rawData.mask.map((val, index) => (
+                    <mask id={`mask${index}`} key={index}>
+                        <path 
+                            fill="none"
+                            stroke="white"
+                            strokeWidth={rawData.maskWidth[index]}
+                            d={val}
+                            pathLength="100"
+                            strokeDasharray="100 100"
+                            strokeDashoffset={offset}
+                        />
+                    </mask>
+                )
+            )}
+        </>
     )
 
     return (
         <Motion
             defaultStyle={{offset: 100}}
             style={{offset: spring(0, presets.stiff)}}
-        >{animatedMask}</Motion>
+        >{animatedMasks}</Motion>
     )
 }
 
